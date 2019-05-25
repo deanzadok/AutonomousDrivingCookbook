9ea5eb2e10f839ac62befbe11ac7fad6e5da231c
Dean Zadok <t-dezado@microsoft.com>
2019-05-24 17:54:25 -0700

    Finished cooking and training scripts

---
+++ AirSimE2EDeepLearning/cook_data.py
@@ -0,0 +1,26 @@
+#%matplotlib inline
+import numpy as np
+import pandas as pd
+import h5py
+from matplotlib import use
+use("TkAgg")
+import matplotlib.pyplot as plt
+from PIL import Image, ImageDraw
+import os
+import Cooking
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--data_dir', '-data_dir', help='path to raw data folder', default='C:\\Users\\t-dezado\\OneDrive - Microsoft\\Documents\\AirSim', type=str)
+parser.add_argument('--output_dir', '-output_dir', help='path to destination folder', default='cooked_data', type=str)
+parser.add_argument('--buffer_size', '-buffer_size', help='number of images in one sample', default=4, type=int)
+parser.add_argument('--img_type', '-img_type', help='type of image from [rgb, depth]', default='depth', type=str)
+args = parser.parse_args()
+# No test set needed, since testing in our case is running the model on an unseen map in AirSim
+train_test_split = [0.9, 0.1]
+
+# list of all recordings data_folders
+data_folders = [name for name in os.listdir(args.data_dir) if os.path.isdir(os.path.join(args.data_dir, name))]
+data_folders = [os.path.join(args.data_dir, f) for f in data_folders]
+
+Cooking.cook(data_folders, args.output_dir, args.buffer_size, args.img_type, train_test_split)
---
+++ AirSimE2EDeepLearning/train.py
@@ -0,0 +1,105 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+import os
+import numpy as np
+import h5py
+import argparse
+import tensorflow as tf
+from tensorflow.keras.layers import Dense, Flatten, Conv2D
+from tensorflow.keras import Model
+
+# model definition class
+class RLModel(Model):
+  def __init__(self):
+    super(RLModel, self).__init__()
+    self.conv1 = Conv2D(filters=16, kernel_size=8, strides=4, activation='relu')
+    self.conv2 = Conv2D(filters=32, kernel_size=4, strides=2, activation='relu')
+    self.conv3 = Conv2D(filters=32, kernel_size=3, strides=1, activation='relu')
+    self.flatten = Flatten()
+    self.d1 = Dense(units=256, activation='relu')
+    self.d2 = Dense(units=args.num_actions, activation='softmax')
+
+  def call(self, x):
+    x = self.conv1(x)
+    x = self.conv2(x)
+    x = self.conv3(x)
+    x = self.flatten(x)
+    x = self.d1(x)
+    return self.d2(x)
+
+# tf function to train
+@tf.function
+def train(images, labels):
+    with tf.GradientTape() as tape:
+        predictions = model(images)
+        loss = loss_object(labels, predictions)
+    gradients = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+    train_loss(loss)
+    train_accuracy(labels, predictions)
+
+# tf function to test
+@tf.function
+def test(images, labels):
+    predictions = model(images)
+    t_loss = loss_object(labels, predictions)
+
+    test_loss(t_loss)
+    test_accuracy(labels, predictions)
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_dir', '-data_dir', help='path to raw data folder', default='C:\\Users\\t-dezado\\OneDrive - Microsoft\\Documents\\Github\\AutonomousDrivingCookbook\\cooked_data', type=str)
+    parser.add_argument('--output_dir', '-output_dir', help='path to output folder', default='C:\\Users\\t-dezado\\OneDrive - Microsoft\\Documents\\Github\\AutonomousDrivingCookbook\\models', type=str)
+    parser.add_argument('--num_actions', '-num_actions', help='number of actions for the model to perdict', default=5, type=int)
+    parser.add_argument('--batch_size', '-batch_size', help='number of samples in one minibatch', default=32, type=int)
+    parser.add_argument('--epochs', '-epochs', help='number of epochs to train the model', default=20, type=int)
+    args = parser.parse_args()
+
+    # upload train and test datasets
+    train_dataset = h5py.File(os.path.join(args.data_dir, 'train.h5'), 'r')
+    x_train = np.asarray(train_dataset['image']) / 255.0
+    y_train = np.asarray(train_dataset['label']).astype(int)
+    x_train = x_train.transpose(0, 2, 3, 1) # NCHW => NHWC
+
+    test_dataset = h5py.File(os.path.join(args.data_dir, 'test.h5'), 'r')
+    x_test = np.asarray(test_dataset['image']) / 255.0
+    y_test = np.asarray(test_dataset['label']).astype(int)
+    x_test = x_test.transpose(0, 2, 3, 1) # NCHW => NHWC
+
+    # convert to tf format dataset and prepare batches
+    train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train)).batch(32)
+    test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(32)
+
+    # create model, loss and optimizer
+    model = RLModel()
+    loss_object = tf.keras.losses.SparseCategoricalCrossentropy()
+    optimizer = tf.keras.optimizers.Adam()
+
+    # define metrics
+    train_loss = tf.keras.metrics.Mean(name='train_loss')
+    train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
+    test_loss = tf.keras.metrics.Mean(name='test_loss')
+    test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='test_accuracy')
+
+    # check if output folder exists
+    if not os.path.isdir(args.output_dir):
+        os.makedirs(args.output_dir)
+
+    # train
+    print('Start training...')
+    for epoch in range(args.epochs):
+        for images, labels in train_ds:
+            train(images, labels)
+
+        for test_images, test_labels in test_ds:
+            test(test_images, test_labels)
+        
+        # save model
+        if epoch % 5 == 0 and epoch > 0:
+            print('Start weights to {}'.format(args.output_dir))
+            model.save_weights(os.path.join(args.output_dir, "model{}.ckpt".format(epoch)))
+        
+        print('Epoch {}, Loss: {}, Accuracy: {}, Test Loss: {}, Test Accuracy: {}'.format(epoch+1, train_loss.result(), train_accuracy.result()*100, test_loss.result(), test_accuracy.result()*100))
