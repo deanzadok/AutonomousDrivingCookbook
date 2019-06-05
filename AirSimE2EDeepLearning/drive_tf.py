4ab74f14044e8eea60fe51cbc075fc2a5f3309e6
Dean Zadok <t-dezado@microsoft.com>
2019-06-04 20:13:24 -0700

    Add grayscale option to tf drive

--- AirSimE2EDeepLearning/drive_tf.py
+++ AirSimE2EDeepLearning/drive_tf.py
@@ -9,46 +9,16 @@ import tensorflow as tf
 from train import RLModel
 from PIL import Image
 from coverage_map import CoverageMap
+import cv2
+import time
+import datetime
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--path', '-path', help='model file path', default='C:\\Users\\t-dezado\\OneDrive - Microsoft\\Desktop\\model25.ckpt', type=str)
+parser.add_argument('--path', '-path', help='model file path', default='C:\\Users\\t-dezado\\OneDrive - Microsoft\\Desktop\\model15.ckpt', type=str)
+parser.add_argument('--camera', '-camera', help='type of the camera. choose from [rgb, depth, grayscale]', default='grayscale', type=str)
+parser.add_argument('--debug', '-debug', dest='debug', help='debug mode, present fron camera on screen', action='store_true')
+parser.add_argument('--store', '-store', dest='store', help='store images to experiment folder', action='store_true')
 args = parser.parse_args()
-
-# Gets a coverage image from AirSim
-def get_cov_image(covmap):
-
-    state, reward = covmap.get_state_from_pose()
-
-    # debug only
-    #print(reward)
-    #im = PIL.Image.fromarray(np.uint8(state))
-    #im.save("DistributedRL\\debug\\{}.png".format(time.time()))
-
-    state = state / 255.0
-    return state
-
-
-def get_depth_image(client):
-
-    responses = client.simGetImages([airsim.ImageRequest("RCCamera", airsim.ImageType.DepthPerspective, True, False)])
-    img1d = np.array(responses[0].image_data_float, dtype=np.float)
-
-    if img1d.size > 1:
-
-        img1d = 255/np.maximum(np.ones(img1d.size), img1d)
-        img2d = np.reshape(img1d, (responses[0].height, responses[0].width))
-
-        image = Image.fromarray(img2d)
-
-        # debug only
-        #image_png = image.convert('RGB')
-        #image_png.save("DistributedRL\\debug\\{}.png".format(time.time()))
-
-        im_final = np.array(image.resize((84, 84)).convert('L')) 
-        im_final = im_final / 255.0
-        return im_final
-
-    return np.zeros((84,84)).astype(float)
 
 # tf function to test
 @tf.function
@@ -59,6 +29,15 @@ if __name__ == "__main__":
 
     # allow growth is possible using an env var in tf2.0
     os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
+
+    if args.store:
+        # create experiments directories
+        experiment_dir = os.path.join('C:\\Users\\t-dezado\\OneDrive - Microsoft\\Documents\\AirSim', datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S"))
+        images_dir = os.path.join(experiment_dir, 'images')
+        os.makedirs(images_dir)
+        # create txt file
+        airsim_rec = open(os.path.join(experiment_dir,"airsim_rec.txt"),"w") 
+        airsim_rec.write("TimeStamp\tPOS_X\tPOS_Y\tPOS_Z\tRPM\tSpeed\tSteering\tImageFile\n") 
 
     # Load the model
     model = RLModel()
@@ -71,13 +50,14 @@ if __name__ == "__main__":
     car_controls = airsim.CarControls()
 
     # let the car start driving
-    car_controls.throttle = 0.3
+    car_controls.throttle = 0.5
     car_controls.steering = 0
     client.setCarControls(car_controls)
 
     # create coverage map and connect to client
-    start_point = [-290.0, 10050.0, 10.0]
-    covMap = CoverageMap(start_point=start_point, map_size=32000, scale_ratio=20, state_size=6000, input_size=20, height_threshold=0.9, reward_norm=30, paint_radius=15)
+    #start_point = [-290.0, 10050.0, 10.0]
+    start_point = [0.0, 0.0, 0.0]
+    covMap = CoverageMap(start_point=start_point, map_size=64000, scale_ratio=20, state_size=6000, input_size=20, height_threshold=0.9, reward_norm=30, paint_radius=15)
     covMap.set_client(client=client)
 
     # actions list
@@ -90,14 +70,56 @@ if __name__ == "__main__":
 
     while(True):
 
+        if args.camera == 'depth':
+            # get depth image from airsim
+            responses = client.simGetImages([airsim.ImageRequest("RCCamera", airsim.ImageType.DepthPerspective, True, False)])
+            img1d = np.array(responses[0].image_data_float, dtype=np.float)
+            img1d = 255/np.maximum(np.ones(img1d.size), img1d)
+            if img1d.size > 1:
+                img2d = np.reshape(img1d, (responses[0].height, responses[0].width))
+                image = Image.fromarray(img2d)
+                image_np = np.array(image.resize((84, 84)).convert('L')) 
+            else:
+                image_np = np.zeros((84,84)).astype(float)
+        else: # args.camera = 'rgb' or 'grayscale'
+            # get image from AirSim
+            image_response = client.simGetImages([airsim.ImageRequest("RCCamera", airsim.ImageType.Scene, False, False)])[0]
+            img1d = np.fromstring(image_response.image_data_uint8, dtype=np.uint8)
+            if img1d.size > 1:
+                img2d = np.reshape(img1d, (image_response.height, image_response.width, 3))
+                image = Image.fromarray(img2d)
+                if args.camera == 'grayscale':
+                    image = image.convert('L')
+                image_np = np.array(image.resize((84, 84)))
+                
+            else:
+                if args.camera == 'grayscale':
+                    image_np = np.zeros((84,84)).astype(float)
+                else:
+                    image_np = np.zeros((84,84,3)).astype(float)
+
+        # get coverage image
+        cov_image, reward = covMap.get_state_from_pose()
+        #print("reward: {}".format(reward))
+
         # combine both inputs
-        cov_image = get_cov_image(covMap)
-        image = get_depth_image(client)
-        image[:cov_image.shape[0],:cov_image.shape[1]] = cov_image
+        if args.camera == 'depth' or args.camera == 'grayscale':
+            image_np[:cov_image.shape[0],:cov_image.shape[1]] = cov_image
+        else:
+            cov_rgb_image = np.expand_dims(cov_image, axis=2)
+            cov_rgb_image = np.repeat(cov_rgb_image, 3, axis=2)
+            image_np[:cov_rgb_image.shape[0],:cov_rgb_image.shape[1],:] = cov_rgb_image
+
+        # present state image if debug mode is on
+        if args.debug:
+            cv2.imshow('navigation map', image_np)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
         # append to buffer
+        image_np = image_np / 255.0
         buffer[:-1] = buffer[1:]
-        buffer[-1] = image
+        buffer[-1] = image_np
         
         # convert input to [1,84,84,4]
         input_img = np.expand_dims(buffer, axis=0).transpose(0, 2, 3, 1).astype(np.float64)
